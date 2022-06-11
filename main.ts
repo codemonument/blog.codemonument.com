6730c670a9f1d1014702a3310b612e2520b2fd57
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-06-11 17:37:57 +0200

    add basic blog customization

--- main.ts
+++ main.ts
@@ -1,23 +1,22 @@
 import blog, { ga, redirects } from "https://deno.land/x/blog@0.3.3/blog.tsx";
 
 blog({
-  title: "My Blog",
+  title: "Code Monuments",
   description: "This is my new blog.",
-  cover: "https://deno-avatar.deno.dev/avatar/blog.svg",
-  coverStyle: "avatar-rounded",
-  author: "An author",
+  cover: "./assets/banner.jpg",
+  // coverStyle: "avatar-rounded",
+  author: "Benjamin Jesuiter",
   background: "#f9f9f9",
-
   // middlewares: [
-    
-    // If you want to set up Google Analytics, paste your GA key here.
-    // ga("UA-XXXXXXXX-X"),
 
-    // If you want to provide some redirections, you can specify them here,
-    // pathname specified in a key will redirect to pathname in the value.
-    // redirects({
-    //  "/hello_world.html": "/hello_world",
-    // }),
+  // If you want to set up Google Analytics, paste your GA key here.
+  // ga("UA-XXXXXXXX-X"),
+
+  // If you want to provide some redirections, you can specify them here,
+  // pathname specified in a key will redirect to pathname in the value.
+  // redirects({
+  //  "/hello_world.html": "/hello_world",
+  // }),
 
   // ]
 });
