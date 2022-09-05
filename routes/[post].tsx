a8d2a32eb56f585f3c15d604b408a92027ba22e2
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-09-05 10:22:19 +0200

    move all blogpost md files into /static folder

--- routes/[post].tsx
+++
@@ -1,7 +0,0 @@
-/** @jsx h */
-import { h } from "preact";
-import { PageProps } from "$fresh/server.ts";
-
-export default function RenderPost(props: PageProps) {
-  return <div>Hello Blogpost {props.params.post}</div>;
-}
