f16f0b7c6bf5d6a89fe7068a34c38b6a7eed70be
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-09-05 10:46:05 +0200

    add basic working post loader!

--- routes/posts/[post].tsx
+++ routes/posts/[post].tsx
@@ -1,7 +1,28 @@
 /** @jsx h */
 import { h } from "preact";
-import { PageProps } from "$fresh/server.ts";
+import { Handlers, PageProps } from "$fresh/server.ts";
+
+interface MdData {
+  rawContent: string;
+}
+
+
+export const handler: Handlers<MdData | null> = {
+  async GET(_, ctx) {
+    const { post } = ctx.params;
+    const content = await Deno.readTextFile(`content/posts/${post}.md`);
+    // const resp = await fetch(`https://api.github.com/users/${username}`);
+    // if (resp.status === 404) {
+    //   return ctx.render(null);
+    // }
+    const mdData: MdData = {rawContent: content}
+    return ctx.render(mdData);
+  },
+};
 
 export default function RenderPost(props: PageProps) {
-  return <div>Hello Blogpost {props.params.post}</div>;
+  return <div>
+    <h1>Post {props.params.post}</h1>
+    <p>{props.data.rawContent}</p>
+  </div>;
 }
