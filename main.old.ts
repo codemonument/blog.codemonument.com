ca1d656b7967d4a8d5aaa8832ee25feb92d3039f
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-09-05 10:07:35 +0200

    add fresh code from starter

---
+++ main.old.ts
@@ -0,0 +1,43 @@
+/** @jsx h */
+
+/** @jsxFrag Fragment */
+/// <reference lib="dom" />
+/// <reference lib="dom.asynciterable" />
+/// <reference lib="deno.ns" />
+
+import blog, { h } from "https://deno.land/x/blog@0.3.3/blog.tsx";
+
+/**
+ * TODO: write a small webcomponent, which loads an image from unsplash and shows attribution directly on top of the image (for example as small grey text)
+ */
+
+blog({
+  title: "Code Monuments",
+  description: "Make the web the best platform it can be!",
+  cover: "./assets/banner.jpeg",
+  // coverStyle: "avatar-rounded",
+  author: "Benjamin Jesuiter",
+  background: "#f9f9f9",
+  coverTextColor: "#f9f9f9",
+  avatar: "./assets/avatar.jpeg",
+  links: [
+    { title: "Email", url: "mailto:blog@codemonument.com" },
+    { title: "GitHub Codemonument", url: "https://github.com/codemonument" },
+    { title: "GitHub bjesuiter", url: "https://github.com/bjesuiter" },
+    { title: "Twitter", url: "https://twitter.com/codemonument" },
+  ],
+  // TODO: Figure out how this header customization works!!!!
+  // header: <div> <h2> Testheader < /h2></div>,
+  // middlewares: [
+
+  // If you want to set up Google Analytics, paste your GA key here.
+  // ga("UA-XXXXXXXX-X"),
+
+  // If you want to provide some redirections, you can specify them here,
+  // pathname specified in a key will redirect to pathname in the value.
+  // redirects({
+  //  "/hello_world.html": "/hello_world",
+  // }),
+
+  // ]
+});
--- main.ts
+++ main.ts
@@ -1,43 +1,26 @@
-/** @jsx h */
-
-/** @jsxFrag Fragment */
+/// <reference no-default-lib="true" />
 /// <reference lib="dom" />
 /// <reference lib="dom.asynciterable" />
 /// <reference lib="deno.ns" />
+/// <reference lib="deno.unstable" />
 
-import blog, { h } from "https://deno.land/x/blog@0.3.3/blog.tsx";
+import { InnerRenderFunction, RenderContext, start } from "$fresh/server.ts";
+import manifest from "./fresh.gen.ts";
 
-/**
- * TODO: write a small webcomponent, which loads an image from unsplash and shows attribution directly on top of the image (for example as small grey text)
- */
+import { config, setup } from "@twind";
+import { virtualSheet } from "twind/sheets";
 
-blog({
-  title: "Code Monuments",
-  description: "Make the web the best platform it can be!",
-  cover: "./assets/banner.jpeg",
-  // coverStyle: "avatar-rounded",
-  author: "Benjamin Jesuiter",
-  background: "#f9f9f9",
-  coverTextColor: "#f9f9f9",
-  avatar: "./assets/avatar.jpeg",
-  links: [
-    { title: "Email", url: "mailto:blog@codemonument.com" },
-    { title: "GitHub Codemonument", url: "https://github.com/codemonument" },
-    { title: "GitHub bjesuiter", url: "https://github.com/bjesuiter" },
-    { title: "Twitter", url: "https://twitter.com/codemonument" },
-  ],
-  // TODO: Figure out how this header customization works!!!!
-  // header: <div> <h2> Testheader < /h2></div>,
-  // middlewares: [
+const sheet = virtualSheet();
+sheet.reset();
+setup({ ...config, sheet });
 
-  // If you want to set up Google Analytics, paste your GA key here.
-  // ga("UA-XXXXXXXX-X"),
+function render(ctx: RenderContext, render: InnerRenderFunction) {
+  const snapshot = ctx.state.get("twind") as unknown[] | null;
+  sheet.reset(snapshot || undefined);
+  render();
+  ctx.styles.splice(0, ctx.styles.length, ...(sheet).target);
+  const newSnapshot = sheet.reset();
+  ctx.state.set("twind", newSnapshot);
+}
 
-  // If you want to provide some redirections, you can specify them here,
-  // pathname specified in a key will redirect to pathname in the value.
-  // redirects({
-  //  "/hello_world.html": "/hello_world",
-  // }),
-
-  // ]
-});
+await start(manifest, { render });
